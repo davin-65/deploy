2ac50f8fb4f81dd9f70897b3a9ca81026bda399b
davin-65 <putradavin2805@gmail.com>
2023-10-24 19:43:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME = os.environ.get("DB_NAME")
+MONGODB_URI = os.environ.get("mongodb+srv://fadil123:fadil123@atlascluster.jbkflub.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("client.pkl2")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
